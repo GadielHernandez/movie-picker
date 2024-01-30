b5c0b02434e037fde09e71232d93782220d785a3
GadielHernandez <gadhdz96@gmail.com>
2024-01-30 16:21:23 -0600

    feat: guest home page

--- src/components/core/Button.tsx
+++ src/components/core/Button.tsx
@@ -8,12 +8,12 @@ interface Props {
 }
 export default function Button({ children, onClick, outline, circle }: Props) {
     let classes =
-        'block text-white bg-amber-500 hover:bg-amber-600 font-medium rounded-lg px-5 py-2.5 text-sm text-center'
+        'block text-white bg-amber-500 hover:bg-amber-600 active:bg-amber-800 font-medium rounded-lg px-5 py-2.5 text-sm text-center'
 
     if (outline)
         classes = classes.replace(
-            'bg-amber-500 hover:bg-amber-600',
-            'bg-transparent hover:bg-gray-200 text-gray-400 hover:text-gray-900'
+            'bg-amber-500 hover:bg-amber-600 active:bg-amber-800',
+            'bg-transparent hover:bg-gray-200 text-gray-400 hover:text-gray-900 active:bg-amber-800'
         )
 
     if (circle)
